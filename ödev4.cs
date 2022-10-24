37f24828f3fec6703266c547f7e4af71d2820102
tugba yazici <yazicittugba@gmail.com>
2022-10-24 20:45:47 +0300

    added

---
+++ ödev4.cs
@@ -0,0 +1,29 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+            try
+            {
+                Console.WriteLine("sayi ; ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(sayi);
+            }
+            catch(Exception error)
+            {
+                 Console.WriteLine(error.Message.ToString());
+            }
+            finally
+            {
+                 Console.WriteLine("finally");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+                
+            }
+            catch (ArgumentException ex)
+            {
+                
+                 Console.WriteLine("Null");
+                  Console.WriteLine(ex);
+            }
