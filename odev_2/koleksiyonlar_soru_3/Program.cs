d1bac2754a1a0ad55414ba8b9707035b806be391
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2022-11-08 00:17:03 +0300

    homework 2 question 3 added

---
+++ odev_2/koleksiyonlar_soru_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace koleksiyonlar_soru_3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle girin:");
+            string? cümle = Console.ReadLine();
+            Console.WriteLine(cümle.ToLower());
+            
+            string ünlüler ="aeıiuüoö";
+            
+            int cümleUzunluğu= cümle.Count();
+
+            List<char> ünlüListe = new List<char>();
+            for (int i = 0; i < cümleUzunluğu; i++)
+            {
+                if(ünlüler.Contains(cümle[i]))
+                {
+                    if (!ünlüListe.Contains(cümle[i]))
+                    {
+                        ünlüListe.Add(cümle[i]);
+                    } 
+                }
+                else
+                    continue; 
+            }
+            Console.Write("Cümledeki ünlü harfler:");    
+            foreach (var item in ünlüListe)
+            {
+                Console.Write(item+",");
+            }        
+        }
+    }
+}
