73f998fdb73c7536faa843427686009073930ec2
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2022-11-08 17:50:49 +0300

    enum added

---
+++ csharp_enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+            int sıcaklık=32;
+            if(sıcaklık<=(int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if (sıcaklık>=(int)HavaDurumu.Sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            }
+            else if (sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.Coksıcak )
+            {
+                Console.WriteLine("Hadi dışarı çıkalım");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sıcak=25,
+        Coksıcak=30
+    }
+}
